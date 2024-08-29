0bc018fcb05e022e413154a16df82a3acadca9b6
Christopher-NguyenCS <69370834+Christopher-NguyenCS@users.noreply.github.com>
2024-08-29 16:19:49 -0400

    - "Sync changes"

--- backend/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b0bc13fe5babb05ea89c83255667abb5a861f79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e098aa9e6b2cd799dbc62a63b519311b629cf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
