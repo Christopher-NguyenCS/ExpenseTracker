f6047661991288264d3c64f4bd8a36b388ca43ae
Christopher-NguyenCS <69370834+Christopher-NguyenCS@users.noreply.github.com>
2024-08-31 18:20:24 -0400

    - Add scroll for TransactionList.jsx - Add data fetching in TransactionList.jsx to get all expenses only once - Adjust the height of the third column(rightmost) - Made height bigger for calendar

--- frontend/src/routes/TransactionList.jsx
+++ frontend/src/routes/TransactionList.jsx
@@ -1,35 +1,41 @@
 import { useState, useEffect } from "react";
+import { Link,redirect } from "react-router-dom";
 import styles from "../styles/transactionList.module.css";
+import { set } from "date-fns";
+
+
+
 
 export default function TransactionList(dates){
-    const [data,setData] = useState([]);
+    const [data,setData] = useState([null]);
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [error,setError] = useState("None");
     let startDate;
     let endDate;
     
-    const getExpenses = async () =>{
-        const response = await fetch("http://localhost:5258/expenses",{
-            method:"GET", 
-            headers:{
-                "Content-Type":"application/json"
-            }
-        });
-        try{
-            if(!response.ok){
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const result = await response.json();
-            setData(result);
-        }catch (error) {
-            setError(error.message);
-          } finally {
-            setLoading(false);
-          }
-        }
+
 
     useEffect(()=>{
+        const getExpenses = async () =>{
+            try{
+                const response = await fetch("http://localhost:5258/expenses",{
+                    method:"GET", 
+                    headers:{
+                        "Content-Type":"application/json"
+                    }
+                });
+                if(!response.ok){
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const result = await response.json();
+                setData(result);
+            }catch (error) {
+                setError(error.message);
+            }finally{setLoading(false)}
+        }
+
         getExpenses();
+        
     },[]);
 
 
@@ -45,15 +51,19 @@ export default function TransactionList(dates){
     return(
         <>
             <div className={styles.container}>
-                List
-                {startDate}
-                <ul>    
-                    {/* {props.expenses.map(expense => (
-                        <li>{expense}</li>
-                    ))}; */}
-                    {data.map(d => {<li>d.title</li>})}
-                </ul>
-                
+
+                <div className={styles.listContainer}>
+                    <header><h2>Transaction History</h2></header>
+                    <ul className={styles.list}>    
+                        {data.length > 0 ? data.map(d => (<li key={d?.id}>{d.title}: ${d.cost}</li>)):<li>None in Transaction history: {data.length}</li>}
+                    </ul>
+                </div> 
+
+                <div className={styles.btnContainer}>
+                    <Link to={"/expenses"}>
+                        <button type="button" className={styles.btn}>View All Transaction</button>
+                    </Link>
+                </div>
             </div>
         </>
     )
